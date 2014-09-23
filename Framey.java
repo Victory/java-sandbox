a131e67244164ac206c95a5fe829c2d4d1dadfed
vic <git@dfhu.org>
2014-09-22 20:00:22 -0400

    make a subclass for WindowListener

--- Framey.java
+++ Framey.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-import javax.swing.JFrame;
+import victory.windowy.VicWindowListener;
 
 
 public class Framey extends Frame implements ActionListener {
@@ -19,49 +19,7 @@ public class Framey extends Frame implements ActionListener {
     setSize(250, 250);
     //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    WindowListener windowListener = new WindowListener () {
-
-      @Override
-      public void windowClosing(WindowEvent arg0) {
-        System.exit(0);
-      }
-
-      @Override
-      public void windowDeactivated(WindowEvent arg0) {
-        // TODO Auto-generated method stub
-
-      }
-
-      @Override
-      public void windowDeiconified(WindowEvent arg0) {
-        // TODO Auto-generated method stub
-
-      }
-
-      @Override
-      public void windowIconified(WindowEvent arg0) {
-        // TODO Auto-generated method stub
-
-      }
-
-      @Override
-      public void windowOpened(WindowEvent arg0) {
-        // TODO Auto-generated method stub
-
-      }
-
-      @Override
-      public void windowActivated(WindowEvent e) {
-        // TODO Auto-generated method stub
-
-      }
-
-      @Override
-      public void windowClosed(WindowEvent e) {
-        // TODO Auto-generated method stub
-
-      }
-    };
+    WindowListener windowListener = new VicWindowListener();
 
     addWindowListener(windowListener);
     setVisible(true);
