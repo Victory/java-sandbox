0aba2e6800c6f79a2c98f6b3e1f4ba156fce3885
vic <git@dfhu.org>
2014-09-23 20:05:00 -0400

    buy auto clicker by clicking button

--- Framey.java
+++ Framey.java
@@ -1,6 +1,10 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JOptionPane;
 
 import victory.windowy.VicWindowListener;
 
@@ -8,8 +12,11 @@ import victory.windowy.VicWindowListener;
 public class Framey extends Frame implements ActionListener {
 
   private Button btn;
+  private Button buyAutoClicker;
+
   private TextField cookieText;
   Integer numCookies = 0;
+  int numAutoClickers = 0;
 
   private static final long serialVersionUID = 1L;
 
@@ -20,18 +27,31 @@ public class Framey extends Frame implements ActionListener {
     btn.addActionListener(this);
     add(btn);
 
+    bindByAutoClicker();
 
     cookieText = new TextField(numCookies.toString(), 5);
     cookieText.setEditable(false);
     cookieText.setText(numCookies.toString());
     add(cookieText);
 
+    add(buyAutoClicker);
+
     addWindowListener(new VicWindowListener());
 
     setSize(250, 250);
     setVisible(true);
+  }
 
-    startAutoClicker();
+  private void bindByAutoClicker() {
+    buyAutoClicker = new Button("Buy Auto Clicker");
+    buyAutoClicker.addActionListener(
+        new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            // TODO Auto-generated method stub
+            startAutoClicker();
+          }
+        });
   }
 
   public static void main(String[] args) {
@@ -49,21 +69,25 @@ public class Framey extends Frame implements ActionListener {
   }
 
   private void startAutoClicker () {
-    Thread thread = new Thread () {
+
+    if (numAutoClickers > 0) {
+      String message = "you already have an auto clicker";
+      JOptionPane.showMessageDialog(this, message);
+      return;
+    }
+    numAutoClickers += 1;
+
+    Timer timer = new Timer(true);
+    TimerTask task = new TimerTask() {
+
       @Override
-      public void run()
-      {
-        while (true) {
-          try {
-            sleep(1000);
-          } catch (InterruptedException e) {
-            return;
-          }
-          incrementCounter(1);
-        }
+      public void run() {
+        incrementCounter(1);
       }
     };
-    thread.setDaemon(true);
-    thread.run();
+
+    long delay = 0;
+    long period = 1000;
+    timer.scheduleAtFixedRate(task, delay, period);
   }
 }
